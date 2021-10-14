1c768d0518a7054251f508ad29568e8365885912
Yurii Didyk <juran2201@gmail.com>
2021-10-14 18:16:29 +0300

    add message function

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,5 @@
 import telebot
-from telebot.types import Message
+from telebot.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
@@ -12,6 +12,24 @@ def start(message: Message) -> None:
                      f'Have a good day!!\n' +
                      f'I\'ll help you download stickers!\n' +
                      f'Send me a sticker and I\'ll download it for you')
+
+
+@bot.message_handler(content_types=['text', 'sticker'])
+def message(message: Message) -> None:
+    if message.text:
+        bot.send_message(message.chat.id, 'You need to send me a sticker‼')
+
+    elif message.sticker:
+        sticker_info = message.sticker
+        inline_markup = InlineKeyboardMarkup(row_width=2).add(
+            InlineKeyboardButton('Download the sticker', callback_data='sticker'),
+            InlineKeyboardButton('Download sticker pack', callback_data='pack')
+        )
+        bot.send_message(message.chat.id,
+                         f'Information about the sticker:\n' +
+                         f'file id: {sticker_info.file_id}\n' +
+                         f'emoji: {sticker_info.emoji}\n' +
+                         f'set name: {sticker_info.set_name}', reply_markup=inline_markup)
 
 
 if __name__ == '__main__':
